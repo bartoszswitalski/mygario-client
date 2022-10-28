b536f34c7cb50c6e89c73faa06f72efb26775780
bswitalski <bartoszswitalski@icloud.com>
2022-10-28 22:34:21 +0200

    Fix build

--- src/board/adapters/board-user-logged-out.event-handler.ts
+++ src/board/adapters/board-user-logged-out.event-handler.ts
@@ -4,6 +4,6 @@ import { BoardUserLoggedOutEvent } from '../../core/events/board-user-logged-out
 export class BoardUserLoggedOutEventHandler implements ApplicationEventHandler {
     eventsClasses = [BoardUserLoggedOutEvent];
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/no-empty-function
     handle(events: BoardUserLoggedOutEvent[]): void {}
 }
